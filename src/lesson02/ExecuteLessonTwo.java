81f0d47282212f1989fcc8b92fdcfb250e26b8ba
serhii-zaitsev <szaicevbrd@gmail.com>
2019-08-22 13:26:00 +0300

    Lesson 02 (String) adding method eachNth

---
+++ src/lesson02/ExecuteLessonTwo.java
@@ -0,0 +1,26 @@
+package lesson02;
+
+public class ExecuteLessonTwo {
+
+    public static String eachNth(String str, int n){
+        int count = 0;
+        char[] mas = str.toCharArray();
+        String res = "" + mas[0];
+        for(int x = 1; x < mas.length - n; x++)
+            {
+                if(count < mas.length - n)
+                    {
+                        res = res + mas[count = count + n];
+                    }
+            }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(eachNth("Maspild",2));
+        System.out.println(eachNth("MaspildA",2));
+        System.out.println(eachNth("MaspildAa",2));
+        System.out.println(eachNth("Abcdefghl",3));
+        System.out.println(eachNth("Abcdefghl",4));
+    }
+}
